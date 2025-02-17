08fb5cd80d6877506c1c65d7f94526e3a6b5c1de
Anton Luo <22301690@live.uem.es>
2025-02-17 22:36:37 +0100

    Instalado firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") version "4.4.2" apply true
 }
 
 android {
@@ -40,5 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
 }
--- app/src/main/java/com/example/aplicacion/Interfaces/Login.java
+++ app/src/main/java/com/example/aplicacion/Interfaces/Login.java
@@ -1,48 +1,33 @@
 package com.example.aplicacion.Interfaces;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.aplicacion.R;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Login extends AppCompatActivity {
     private EditText etUsuarioLogin;
     private EditText etContrasenaLogin;
-    private EditText tvRegistrateLogin;
+    private TextView tvRegistrateLogin;
     private Button btIniciarSesion;
+    private FirebaseAuth mAuth;
 
-    // See: https://developer.android.com/training/basics/intents/result
-    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            result -> onSignInResult(result)
-    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseApp.initializeApp(this);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -52,49 +37,52 @@ public class Login extends AppCompatActivity {
         });
         etUsuarioLogin = findViewById(R.id.etUsuarioLogin);
         etContrasenaLogin = findViewById(R.id.etContrasenaLogin);
-        //tvRegistrateLogin = findViewById(R.id.tvRegistrateLogin);
+        tvRegistrateLogin = findViewById(R.id.tvRegistrateLogin);
         btIniciarSesion = findViewById(R.id.btIniciarSesionLogin);
 
-        // Comprobar si el usuario ya está autenticado
-        FirebaseUser usuarioActual = FirebaseAuth.getInstance().getCurrentUser();
-        if (usuarioActual != null) {
-            // Si ya ha iniciado sesión, ir a la pantalla principal
-            irAMainPage();
-        }
-        // Asignar el evento al botón
-        btIniciarSesion.setOnClickListener(this::iniciarSesion);
+        FirebaseApp.initializeApp(this);
+        //Cogemos todos los paquetes de firebaseAuthentication
+        mAuth = FirebaseAuth.getInstance();
+
+        btIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = etUsuarioLogin.getText().toString().trim();
+                String password = etContrasenaLogin.getText().toString().trim();
+
+                if (!emailText.isEmpty() && !password.isEmpty()) {
+                    signInWithEmail(emailText, password);
+                }else{
+                    Toast.makeText(Login.this, "Por favor, ingresa un email o contraseña. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        tvRegistrateLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = etUsuarioLogin.getText().toString().trim();
+                String password = etContrasenaLogin.getText().toString().trim();
+
+                if (!emailText.isEmpty() && !password.isEmpty()) {
+                    registrarUsuario(emailText, password);
+                }else{
+                    Toast.makeText(Login.this, "Por favor, ingresa un email o contraseña. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void signInWithEmail(String emailText, String password){
+        mAuth.signInWithEmailAndPassword(emailText,password);
     }
 
-    public void iniciarSesion(View view){
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-        );
-
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setLogo(R.drawable.ic_launcher_foreground) // Opcional: tu logo
-                .setTheme(R.style.Theme_Aplicacion) // Opcional: tu estilo
-                .build();
-        signInLauncher.launch(signInIntent);
-
-        /*Intent intent = new Intent();
+    public void registrarUsuario(String emailText, String password){
+        mAuth.createUserWithEmailAndPassword(emailText,password);
+    }
+   /* public void iniciarSesion(View view){
+        Intent intent = new Intent();
         intent.putExtra("usuario", etUsuarioLogin.getText().toString());
         intent.putExtra("contrasena", etContrasenaLogin.getText().toString());
-        setResult(RESULT_OK, intent);*/
-    }
+        setResult(RESULT_OK, intent);
+    }*/
 
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
-        if (result.getResultCode() == RESULT_OK) {
-            irAMainPage();
-        } else {
-            Toast.makeText(this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
-        }
-    }
-    // Método para ir a la pantalla principal
-    private void irAMainPage() {
-        startActivity(new Intent(this, MainPage.class));
-        finish();
-    }
 }
