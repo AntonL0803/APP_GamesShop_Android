f45666db0df359dd03f47351624efbc9483bccaa
Huihui <huiz0517@gmail.com>
2025-02-27 11:51:45 +0100

    intento google login 1

--- app/src/main/java/com/example/aplicacion/Interfaces/Login.java
+++ app/src/main/java/com/example/aplicacion/Interfaces/Login.java
@@ -32,11 +32,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
-    private EditText etUsuarioLogin;
-    private EditText etContrasenaLogin;
+    private EditText etUsuarioLogin, etContrasenaLogin;
     private TextView tvRegistrateLogin;
     private Button btIniciarSesion;
-    private Button btIniciarSesionGoogle;
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
     private SignInButton googleSignInButton;
@@ -46,19 +44,19 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_login);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         etUsuarioLogin = findViewById(R.id.etUsuarioLogin);
         etContrasenaLogin = findViewById(R.id.etContrasenaLogin);
         tvRegistrateLogin = findViewById(R.id.tvRegistrateLogin);
         btIniciarSesion = findViewById(R.id.btIniciarSesionLogin);
-        //btIniciarSesionGoogle = findViewById(R.id.btIniciarSesionGoogle);
         googleSignInButton = findViewById(R.id.btIniciarSesionGoogle);
 
         FirebaseApp.initializeApp(this);
@@ -66,26 +64,7 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         // Botón de inicio de sesión
-        btIniciarSesion.setOnClickListener(view -> {
-            String email = etUsuarioLogin.getText().toString().trim();
-            String password = etContrasenaLogin.getText().toString().trim();
-
-            if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(Login.this, "Por favor, ingresa un email y contraseña.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                Toast.makeText(Login.this, "Por favor, ingresa un email válido.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (password.length() < 6) {
-                Toast.makeText(Login.this, "La contraseña debe tener al menos 6 caracteres.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            signInWithEmail(email, password);
-        });
+        btIniciarSesion.setOnClickListener(view -> signInWithEmail());
 
         // Redirección al registro
         tvRegistrateLogin.setOnClickListener(view -> {
@@ -94,31 +73,47 @@ public class Login extends AppCompatActivity {
         });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)) // Asegúrate de que este ID sea correcto
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignInButton.setOnClickListener(view -> signInWithGoogle());
 
-        // Botón de Google Sign-In
+        /*// Botón de Google Sign-In
         //btIniciarSesionGoogle.setOnClickListener(v -> signInWithGoogle());
         // Configura el botón de Google Sign-In
         googleSignInButton.setSize(SignInButton.SIZE_STANDARD);  // Puedes elegir entre STANDARD o ICON_ONLY
         googleSignInButton.setOnClickListener(view -> {
             signInWithGoogle();
-        });
+        });*/
     }
 
-    private void signInWithEmail(String email, String password) {
+    private void signInWithEmail() {
+        String email = etUsuarioLogin.getText().toString().trim();
+        String password = etContrasenaLogin.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(Login.this, "Por favor, ingresa un email y contraseña.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(Login.this, "Por favor, ingresa un email válido.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(Login.this, "La contraseña debe tener al menos 6 caracteres.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         Toast.makeText(Login.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
                         // Redirigir a la pantalla principal
-                        Intent intent = new Intent(Login.this, MainPage.class);
-                        intent.putExtra("Email", email); // Use the email parameter
-                        startActivity(intent);
-                        finish(); // Cerrar la actividad de login
+                        intentAlMain();
                     } else {
                         // Manejo de errores
                         if (task.getException() instanceof FirebaseNetworkException) {
@@ -151,7 +146,7 @@ public class Login extends AppCompatActivity {
                 }
             } catch (ApiException e) {
                 Log.w("GoogleSignIn", "Google sign-in fallido", e);
-                Toast.makeText(this, "Error de inicio de sesión con Google: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Error de inicio de sesión con Google. " + e.getMessage(), Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -161,20 +156,21 @@ public class Login extends AppCompatActivity {
         mAuth.signInWithCredential(GoogleAuthProvider.getCredential(account.getIdToken(), null))
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        if (user != null) {
-                            Toast.makeText(Login.this, "Inicio de sesión exitoso con Google.", Toast.LENGTH_SHORT).show();
-                            // Redirigir a la página principal
-                            Intent intent = new Intent(Login.this, MainPage.class);
-                            startActivity(intent);
-                            finish();
-                        }
+                        Toast.makeText(Login.this, "Inicio de sesión exitoso con Google.", Toast.LENGTH_SHORT).show();
+                        // Redirigir a la página principal
+                        intentAlMain();
                     } else {
                         Toast.makeText(Login.this, "Error al autenticar con Firebase.", Toast.LENGTH_LONG).show();
                     }
                 });
     }
+    private void intentAlMain() {
+        Intent intent = new Intent(Login.this, MainPage.class);
+        startActivity(intent);
+        finish(); // Cerrar la actividad de login
+    }
 }
+
         /*btIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
