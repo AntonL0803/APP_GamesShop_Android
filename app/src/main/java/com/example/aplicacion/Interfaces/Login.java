4bc597894572e7b6ee607e9b14b2e441581da43f
Huihui <huiz0517@gmail.com>
2025-03-20 02:09:18 +0100

    terminar de comentar todo el código

--- app/src/main/java/com/example/aplicacion/Interfaces/Login.java
+++ app/src/main/java/com/example/aplicacion/Interfaces/Login.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
